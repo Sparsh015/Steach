0490126d4117c987ec32747d37b9a27619a3d9e5
sparsh15 <sparsh.1518@gmail.com>
2025-12-29 23:41:06 +0530

    urls initital

--- Home/Student/views.py
+++ Home/Student/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def add_student(request):
+    return render(request, "students/add-student.html")
+
+def student_list(request):
+    return render(request, "students/students.html")
+
+def edit_student(request):
+    return render(request, "students/edit-student.html")
+
+def view_student(request):
+    return render(request, "students/student-details.html")
