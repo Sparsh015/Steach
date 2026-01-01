6fa8a88bd51b4d69fe794ce04ab9f717ca89c892
sparsh15 <sparsh.1518@gmail.com>
2026-01-01 23:38:06 +0530

    initial urls and views set

--- Home/Student/urls.py
+++ Home/Student/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.student_dashboard, name='student_dashboard'),
+    path('', views.student_list, name='student_dashboard'),
     path('add-student/', views.add_student, name='add_student'),
     
 ]
