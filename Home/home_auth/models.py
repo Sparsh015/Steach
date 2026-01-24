ea9f4963896db035b9b6bdd81692d94e3297e958
sparsh15 <sparsh.1518@gmail.com>
2026-01-24 23:24:47 +0530

    admin customuser admin

--- Home/home_auth/models.py
+++ Home/home_auth/models.py
@@ -1,7 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.utils.crypto import get_random_string
+from django.utils import timezone  
+from django.core.mail import send_mail
+from django.conf import settings
+import uuid
 # Create your models here.
-class CustomUser(models.Model):
+
+class CustomUser(AbstractUser):
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30)
@@ -11,5 +17,38 @@ class CustomUser(models.Model):
     is_student = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
     is_teacher = models.BooleanField(default=False)
+    
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name="custom_users",
+        blank=True,
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name="custom_users",
+        blank=True,
+    )
+
     def __str__(self):
         return self.username
+    
+class PasswordResetRequest(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    email = models.EmailField()
+    token = models.CharField(max_length=32, default = get_random_string(32), editable=False, unique = True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    TOKEN_VALIDITY_PERIOD = timezone.timedelta(hours=1)
+
+    def is_valid(self):
+        return timezone.now() < self.created_at + self.TOKEN_VALIDITY_PERIOD
+    
+    def send_reset_email(self):
+        reset_link = f"http://localhost:8000/reset-password/{self.token}/"
+        send_mail(
+            'Password Reset Request',
+            f'Click the link to reset your password: {reset_link}',
+            settings.DEFAULT_FROM_EMAIL,
+            [self.email],
+            fail_silently=False,
+        )
