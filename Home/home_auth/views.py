e7d616000bb7585fb1cc6d1effd867436b57c3cd
sparsh15 <sparsh.1518@gmail.com>
2026-01-26 23:59:30 +0530

    views def signup

--- Home/home_auth/views.py
+++ Home/home_auth/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
-
+from django.contrib.auth import login
+from django.shortcuts import redirect
+from django.contrib import messages
+from .models import CustomUser as User
 # Create your views here.
+def signup_view(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        role = request.POST['role']
+
+    #create the user
+    user = User.objects.create_user(
+        email=email,
+        first_name=first_name,
+        last_name=last_name,
+        password=password,
+        role=role,
+    )
+    
+    if role == 'student':
+        user.is_student = True
+    elif role == 'teacher':
+        user.is_teacher = True
+    elif role == 'admin':
+        user.is_admin = True
+    
+    user.save()
+    login(request,user)
+    messages.success(request, 'Account created successfully.')
+    return redirect('index')
+
+    return render(request, 'authentication/register.html')
