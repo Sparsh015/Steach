a697d5753af54bbcf910d7cef38a6d8f405d474b
sparsh15 <sparsh.1518@gmail.com>
2026-01-29 23:55:06 +0530

    restored(extra_commit)

--- Home/home_auth/models.py
+++ Home/home_auth/models.py
@@ -13,7 +13,6 @@ class CustomUser(AbstractUser):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     is_authorised = models.BooleanField(default=False)
-    
     is_student = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
     is_teacher = models.BooleanField(default=False)
